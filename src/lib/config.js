83a59be41afee1999185b6437c329df9c0055658
Ben <menubar@protonmail.com>
2018-10-29 09:17:44 -0400

    new

--- src/lib/config.js
+++ src/lib/config.js
@@ -6,9 +6,9 @@ export const LOGIN_PATH = '/login'
 //  ? 'http://localhost:5000/graphql'
 //  : '/graphql'
 export const GRAPHQL_ENDPOINT = 'https://api.contentkit.co/graphql'
-export const AWS_BUCKET_URL = 'https://s3.amazonaws.com/contentkit-dev-deploymentbucket-3hlcbqx9f9nl'
+export const AWS_BUCKET_URL = 'https://s3.amazonaws.com/contentkit'
 export const AWS_REGION = 'us-east-1'
-export const AWS_BUCKET_NAME = 'contentkit-dev-deploymentbucket-3hlcbqx9f9nl'
+export const AWS_BUCKET_NAME = 'contentkit'
 export const IDENTITY_POOL_ID = 'us-east-1:3c213516-e016-45c8-a3a9-92b4e133a8f4'
 
 export default {
