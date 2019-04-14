b0e975307c13487ff45b4b3f7098ae76c6ba46d9
Ben Gardner <bgardner@deloitte.com>
2019-04-14 12:39:07 -0400

    Updates

--- bin/vault.sh
+++ bin/vault.sh
@@ -6,4 +6,4 @@ touch ~/.aws/credentials
 curl -d '{ "ttl": "3600s" }' \
   -H "X-Vault-Token: $VAULT_TOKEN" \
   https://vault.contentkit.co/v1/aws/creds/ApexUp \
-  | jq -r '.data | "[default]\naws_access_key_id = \(.access_key)\naws_secret_access_key = \(.secret_key)"' > ~/.aws/credentials1
+  | jq -r '.data | "[default]\naws_access_key_id = \(.access_key)\naws_secret_access_key = \(.secret_key)"' > ~/.aws/credentials
